40254ff7fa144df47bea8357a5d5474505ce48a7
Michael Qin <1mike12@gmail.com>
2022-09-02 12:20:23 -0400

    add documentation

--- crypto.js
+++ crypto.js
@@ -1,7 +1,10 @@
 const crypto = require('crypto')
 const OUTPUT_ENCODING = "base64"
+
 module.exports = {
     /**
+     * Creates cyphertext from given raw input and password. The IV, salt, and encrypted text are new-line delimited
+     * and stored as base64 representations
      * @param raw
      * @param password
      * @returns string
@@ -13,21 +16,26 @@ module.exports = {
         const cipher = crypto.createCipheriv("aes-256-ctr", key, iv)
         let encryptedData = cipher.update(raw, 'utf8', OUTPUT_ENCODING)
         encryptedData += cipher.final(OUTPUT_ENCODING)
-        return `${iv.toString('base64')}\n${salt.toString('base64')}\n${encryptedData}`
+        return `${iv.toString(OUTPUT_ENCODING)}\n${salt.toString(OUTPUT_ENCODING)}\n${encryptedData}`
     },
+    /**
+     * @param {string} bundleText
+     * @param {string} password
+     * @returns {string}
+     */
     decode(bundleText, password){
         const [ivString, saltString, encrypted] = bundleText.split("\n")
-        const iv = Buffer.from(ivString, "base64")
-        const salt = Buffer.from(saltString, "base64")
+        const iv = Buffer.from(ivString, OUTPUT_ENCODING)
+        const salt = Buffer.from(saltString, OUTPUT_ENCODING)
         const decipher = crypto.createDecipheriv("aes-256-ctr", this.hashPass(password, salt), iv)
         let decryptedData = decipher.update(encrypted, OUTPUT_ENCODING, 'utf8')
         decryptedData += decipher.final('utf8')
         return decryptedData
     },
     /**
-     * @param utf8String
-     * @param salt Buffer 16
-     * @return Buffer
+     * @param {string} utf8String
+     * @param {Buffer16} salt
+     * @return {Buffer} Buffer
      */
     hashPass(utf8String, salt){
         if (!salt) throw new Error("no salt")
--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const fs = require("fs")
 const crypto = require("./crypto")
 
 module.exports = {
+    /**
+     * encrypts a file with the given password
+     * @param {string} inPath - unencrypted file path
+     * @param {string} outPath - encrypted path to be written to
+     * @param {string} password
+     */
     encodeSync(inPath, outPath, password){
         let text = fs.readFileSync(inPath)
         let encrypted = crypto.encode(text, password)
         fs.writeFileSync(outPath, encrypted, "utf-8")
     },
+    /**
+     * reads an encoded file and returns string representation of file
+     * @param {string} path
+     * @param {string} password
+     * @returns {string}
+     */
     decodeSync(path, password){
         let encrypted = fs.readFileSync(path, "utf-8")
         let decrypted = crypto.decode(encrypted, password)
         return decrypted
     },
+    /**
+     * assigns key value pairs to the global `process.env` object
+     * @param json key value pairs to write
+     * @param allowOverwrite - if true, allows new keys to overwrite pre-existing keys already in `process.env`
+     */
     loadJSONToEnv(json, allowOverwrite = true){
         const existingEnvironmentKeys = new Set(Object.keys(process.env))
 
@@ -26,11 +43,23 @@ module.exports = {
             process.env[key] = value
         }
     },
+    /**
+     * reads an encrypted file from the filesystem with the given password
+     * @param {string} envFilePath
+     * @param {string} password
+     * @returns {json} json
+     */
     getJSON(envFilePath, password){
         let decoded = this.decodeSync(envFilePath, password)
         let json = JSON.parse(decoded)
         return json
     },
+    /**
+     * load an environment file directly to process.env
+     * @param {string} envFilePath
+     * @param {string} password
+     * @param {boolean} overWrite
+     */
     loadJSON(envFilePath, password, overWrite = true){
         let json = this.getJSON(envFilePath, password)
         this.loadJSONToEnv(json, overWrite)
